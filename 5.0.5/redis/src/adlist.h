df20e6be141900314506bbe225453177c94aabab
yanping <yanping@lizhi.fm>
2020-11-27 17:23:14 +0800

    标注链表头文件;

--- 5.0.5/redis/src/adlist.h
+++ 5.0.5/redis/src/adlist.h
@@ -32,38 +32,70 @@
 #define __ADLIST_H__
 
 /* Node, List, and Iterator are the only data structures used currently. */
-
+/**
+ * 双端链表节点;
+ */
 typedef struct listNode {
+    /*前置节点*/
     struct listNode *prev;
+    /*后置节点*/
     struct listNode *next;
+    /*节点的值*/
     void *value;
 } listNode;
 
+/**
+ * 双向链表迭代器;
+ * */
 typedef struct listIter {
+    /*当前迭代到的节点*/
     listNode *next;
+    /*迭代的方向*/
     int direction;
 } listIter;
 
+/**
+ * 双向链表结构;
+ */
 typedef struct list {
+    /*链表头部节点*/
     listNode *head;
+    /*链表尾部节点*/
     listNode *tail;
+
+    /*节点值复制函数*/
     void *(*dup)(void *ptr);
+
+    /*节点值释放函数*/
     void (*free)(void *ptr);
+
+    /*节点值对比函数*/
     int (*match)(void *ptr, void *key);
+
+    /*链表所包含的节点数量*/
     unsigned long len;
 } list;
 
 /* Functions implemented as macros */
+/*返回给定链表所包含的节点数量, T=O(1)*/
 #define listLength(l) ((l)->len)
+/*返回给定链表的表头节点,T=O(1)*/
 #define listFirst(l) ((l)->head)
+/*返回给定链表的尾部节点,T=O(1)*/
 #define listLast(l) ((l)->tail)
+/*返回给点节点的前置节点,T=O(1)*/
 #define listPrevNode(n) ((n)->prev)
+/*返回给点节点的后置节点,T=O(1)*/
 #define listNextNode(n) ((n)->next)
+/*返回给点节点的值,T=O(1)*/
 #define listNodeValue(n) ((n)->value)
 
-#define listSetDupMethod(l,m) ((l)->dup = (m))
-#define listSetFreeMethod(l,m) ((l)->free = (m))
-#define listSetMatchMethod(l,m) ((l)->match = (m))
+/*将链表l的值复制函数设置为m,T=O(1)*/
+#define listSetDupMethod(l, m) ((l)->dup = (m))
+/*将链表l的值释放函数设置为m,T=O(1)*/
+#define listSetFreeMethod(l, m) ((l)->free = (m))
+/*将链表l的对比函数设置为m,T=O(1)*/
+#define listSetMatchMethod(l, m) ((l)->match = (m))
 
 #define listGetDupMethod(l) ((l)->dup)
 #define listGetFree(l) ((l)->free)
@@ -71,25 +103,45 @@ typedef struct list {
 
 /* Prototypes */
 list *listCreate(void);
+
 void listRelease(list *list);
+
 void listEmpty(list *list);
+
 list *listAddNodeHead(list *list, void *value);
+
 list *listAddNodeTail(list *list, void *value);
+
 list *listInsertNode(list *list, listNode *old_node, void *value, int after);
+
 void listDelNode(list *list, listNode *node);
+
 listIter *listGetIterator(list *list, int direction);
+
 listNode *listNext(listIter *iter);
+
 void listReleaseIterator(listIter *iter);
+
 list *listDup(list *orig);
+
 listNode *listSearchKey(list *list, void *key);
+
 listNode *listIndex(list *list, long index);
+
 void listRewind(list *list, listIter *li);
+
 void listRewindTail(list *list, listIter *li);
+
 void listRotate(list *list);
+
 void listJoin(list *l, list *o);
 
-/* Directions for iterators */
+/* Directions for iterators
+ * 迭代器进行迭代的方向
+ * */
+/* 从表头向表尾进行迭代 */
 #define AL_START_HEAD 0
+/* 从表尾向表头进行迭代 */
 #define AL_START_TAIL 1
 
 #endif /* __ADLIST_H__ */
